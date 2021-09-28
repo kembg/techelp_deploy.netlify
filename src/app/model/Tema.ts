8aac100f0cc8bdfac51c034aa350f2e3ab255bc0
kembg <kennedymoises2@gmail.com>
2021-09-27 23:26:36 -0300

    passos para deploy

--- src/app/model/Tema.ts
+++
@@ -1,9 +0,0 @@
-import { Postagem } from "./Postagem"
-
-export class Tema{
-    public id: number
-    public tipo: string
-    public imagem: string
-    public descricao: string
-    public postagens: Postagem[]
-}
